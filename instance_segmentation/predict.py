c772f60f8ad57ce68686f82854e88e86f57bb2a9
FubingZhang <fjfzzzffbb@gmail.com>
2024-01-22 15:16:25 +0800

    Changes to be committed: 	new file:   instance_segmentation/predict.py 	modified:   object_detectron/predict.py

--- instance_segmentation/predict.py
+++ instance_segmentation/predict.py
@@ -6,30 +6,32 @@ from detectron2.config import get_cfg
 from detectron2.data import MetadataCatalog
 from detectron2.engine.defaults import DefaultPredictor
 import cv2
+import time
 
-
-# 读取测试图像
-im = cv2.imread("./test.jpg")
+im = cv2.imread("test.png")
 
 cfg = get_cfg()
-cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_1x.yaml"))  # 使用之前训练的配置文件
-cfg.MODEL.WEIGHTS = "./model_final.pth"  # 使用之前训练得到的模型权重文件
-
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.9
+cfg.merge_from_file("./detectron2_dc/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.8  # set threshold for this model
+cfg.MODEL.WEIGHTS = "./output/bean1_mask_rcnn_R_50_FPN_3x_500iter/model_final.pth"
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
-
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2
 predictor = DefaultPredictor(cfg)
+t1=time.time()
 outputs = predictor(im)
+t2=time.time()
+print(t2-t1)
 
+
+print(outputs["instances"].pred_classes)
 print(outputs["instances"].pred_boxes)
 
 # 创建自定义的 MetadataCatalog
 my_metadata = MetadataCatalog.get("my_dataset")
-my_metadata.set(thing_classes=["plant"])  # 设置自定义的类别名称列表
+my_metadata.set(thing_classes=["leaf"])  # 设置自定义的类别名称列表
 
-# We can use `Visualizer` to draw the predictions on the image.
 v = Visualizer(im[:, :, ::-1], my_metadata, scale=1.0)
 out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
 out_frame=out.get_image()[:, :, ::-1]
+
 cv2.imshow(out_frame)
