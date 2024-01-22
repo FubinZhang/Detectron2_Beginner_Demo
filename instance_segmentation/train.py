ffe2dcc5fdb191eedd4bae8a238f3a04a40eb75a
FubingZhang <fjfzzzffbb@gmail.com>
2024-01-22 14:31:21 +0800

    new file:   instance_segmentation/train.py 	modified:   object_detectron/train.py

---
+++ instance_segmentation/train.py
@@ -0,0 +1,47 @@
+import os
+import numpy as np
+from detectron2.config import get_cfg
+from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.engine import DefaultTrainer
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+from detectron2.data.datasets import register_coco_instances
+from detectron2 import model_zoo
+import torch
+
+#set up cuda device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+register_coco_instances("leaftrain", {}, "./leaf_sets/train/beantrain_coco.json", "./leaf_sets/train")
+
+
+cfg = get_cfg()
+cfg.merge_from_file("./detectron2_dc/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+cfg.DATASETS.TRAIN = ("leaftrain",)
+cfg.DATASETS.TEST = ()
+MetadataCatalog.get("leaftrain").set(thing_classes=["leaf"])
+cfg.DATALOADER.NUM_WORKERS = 2
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
+cfg.SOLVER.IMS_PER_BATCH = 2
+cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+cfg.SOLVER.MAX_ITER = 100    # 300 iterations
+cfg.SOLVER.STEPS = []        # do not decay learning rate
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE =  128   # faster, and good enough for this toy dataset (default: 512)
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2  #the number of classes
+cfg.OUTPUT_DIR = './output/bean1_mask_rcnn_R_50_FPN_3x_500iter'
+cfg.MODEL.DEVICE = "cuda"
+
+
+# 训练器定义
+class MyTrainer(DefaultTrainer):
+  @classmethod
+  def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+    if output_folder is None:
+      output_folder = os.path.join(cfg.OUTPUT_DIR,"inference")
+    return COCOEvaluator(dataset_name, cfg, True, output_folder)
+
+
+
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+trainer = MyTrainer(cfg)
+trainer.resume_or_load(resume=False)
+trainer.train()
